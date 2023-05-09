b1f4f23f667b63f40da9dc9beee48dda4d569286
Cgiff9 <cgiff9@gmail.com>
2023-05-09 08:20:29 -0500

    nfa readme

---
+++ nfa/README.md
@@ -0,0 +1,212 @@
+
+NFA
+===========
+This program reads a nondeterministic finite automaton
+from a supplied "machine file" and runs the automaton 
+on a supplied input string. Automaton only works
+over the alphabet {0, 1, e}, where 'e' is the empty string.
+
+Every character other than zero is treated as a 1. 
+Because the input string is treated as an array of 
+characters, an input string containing the 
+sequence "\0" will be treated the same as the 
+sequence "10". Please be aware of your shell's
+quirks when supplying input strings that contain
+certain special characters other than 0 and 1.
+
+BUILDING
+--------------
+```
+cd automata/nfa
+make nfa
+```
+
+PARAMETERS
+----------
+```
+-q           quiet; turn off printout and delays
+                *program returns 1 on accept, 0 on reject
+-s           convert input to ascii binary representation
+-a <num>     delay printout on string accept by 'num' ms
+-r <num>     delay printout on string reject by 'num' ms
+-f <file>    input string file
+                *may contain multiple strings, one per line
+                *option overridden by command-line input string
+-o <file>    output accept file
+                *file is created if it doesn't exist
+```
+
+The machine file and input string are supplied as such:
+```
+./nfa machineFile.txt 10001
+```
+Other parameters can be mixed throughout:
+```
+./nfa -q machineFile.txt -a600 -r100 10001 -o /tmp/acc.txt
+```
+Just know that the first "non-option" parameter is always
+the machine file and the 2nd "non-option" parameter is 
+always the input string. The input string supplied directly
+via the command line always overrides the '-f' option. 
+
+RUNNING THE MACHINE
+-------------------------
+The input string can be supplied directly as a
+command line argument as shown below:
+```
+$./nfa <machine file> <input string>
+$./nfa auto_EndsTwoOnes.txt 10001011
+```
+The input string can also be supplied in a file containing
+one input string per line.
+```
+$./nfa <machine file> -f <input string file>
+$./nfa auto_EndsTwoOnes.txt -f input_test_string.txt
+```
+Not that if an input string is supplied via the command-line 
+while also invoking the '-f' option, then the '-f'option 
+will be ignored.
+
+MACHINE FILE FORMAT
+-------------------------
+The machine file contains a list of states with one
+state per line. It may contain commented lines starting
+with the hash '#' character, but lines containing states
+may not contain any comments. Blank lines are ignored.
+
+The supplied machine file must follow a specific
+format. For instance, the file "auto_endsTwoOnes.txt"
+contains:
+```
+NAME q0 START 1 FINAL 0 ZERO q0 ONE q0,q1 EMPTY x                                                                NAME q1 START 0 FINAL 0 ZERO x ONE q2 EMPTY x                                                                    NAME q2 START 0 FINAL 1 ZERO x ONE x EMPTY x 
+```
+Each line corresponds to a distinct state which
+contains the following information:
+```
+NAME   : Provide non-spaced name for state
+START* : 1 if this is the start state, else 0
+FINAL  : 1 if this is a final state, else 0
+ZERO   : Upon reading '0', go to state names
+ONE    : Upon reading '1', go to state names
+EMPTY  : Upon reading the empty string, go to state names
+*One and only one START state must exist.
+```
+
+For example, in the "auto_endsTwoOnes.txt" file, the
+first line tells us that there is a state named "q0",
+which is the START state, but is not the FINAL state:
+
+If "q0" reads a '0', it goes to "q0". <br />
+If "q0" reads a '1', it goes to either "q0" or q1.
+
+The second line tells us that there is a state named
+"q1":
+
+If "q1" reads a '0', it does nothing. <br />
+If "q1" reads a '1', it goes to "q2".
+
+The third state contains no transitions. An 'x' or an
+'X' must be used to indicate that no transition
+exists for the particular character.
+
+Multiple state names can be indicated by separting
+them with commas with no spaces between.
+
+Take care not to use the same name for two
+states, or the machine will complain about
+the duplicates.
+
+INPUT STRING FILE FORMAT
+------------------------------
+If an input string file is used, then it may contain 
+an uninterrupted string of '0's and '1's. For example, 
+the input file  called  "input_test_string.txt" looks 
+like this:
+
+```
+111010000101010100010101111100000001010111
+```
+
+This file may contain multiple strings, with
+one per line. Linefeed and newline characters
+will not be included in the string (keep in mind
+this does not refer to the separate characters
+'\' and '\n' together, but to simply pressing
+the 'ENTER/RETURN' button. 
+
+Characters other than linefeeds and newlines will
+be interpreted as '1's, including spaces and tabs.
+
+Other examples that are tested to work on Linux:
+
+```
+./nfa auto_endsTwoOnes.txt -f \
+     input_test_string_multi.txt
+```
+```
+./nfa auto_endsTwoOnes.txt -f \
+     <(cat /dev/urandom | tr -dc '[:alnum:]' | \
+     fold -w 4096 | \
+     sed 's/[a-zA-X1-9]/1/g' | \
+     tr 01 10)
+```
+```
+./nfa auto_endsTwoOnes.txt -f <(cat <<EOF
+110001
+1010101
+10001011
+EOF
+)
+```
+Note that at least on Linux, you may provide files
+that produce an infinite number of lines, like the
+random device /dev/urandom. Keep in mind you will 
+need to halt the program manually with CTRL+C or
+by killing the ./nfa process.
+
+READING OUTPUT
+--------------------
+Here is a sample run with typical program output. 
+This information is displayed unless the '-q'
+option is invoked:
+```
+$ ./nfa auto_endsTwoOnes.txt 10011
+q0, START? 1, FINAL? 0, ZERO-> q0, ONE-> q0,q1, EMPTY->
+q1, START? 0, FINAL? 0, ZERO->  ONE-> q2, EMPTY->
+q2, START? 0, FINAL? 1, ZERO->  ONE->  EMPTY->
+[1]0011: q0 -> q0
+[0]011: q0 -> q0
+[0]11: q0 -> q0
+[1]1: q0 -> q0
+[1]: q0 -> q0
+[1]: q0 -> q1
+[1]1: q0 -> q1
+[1]: q1 -> q2 (F)
+[1]0011: q0 -> q1
+(ACCEPTED):
+        ==>10011
+```
+
+First, the machine file itself is printed.
+
+Second, the transitions made during the machine run
+are output line-by-line. The "(F)" indicates that the 
+latest transition has landed on a FINAL state. The output
+reflected the current character being read in brackets
+followed by the remaining characters to be read. So long
+as the final character leads to a FINAL state, the string
+will be accepted.
+
+Optional delay parameters can be used to pause the
+output on an accepted state and/or a rejected state.
+For example:
+
+```
+./nfa auto_endsTwoOnes.txt 110101 -a500 -r40
+```
+
+These options will delay the output for 500 
+milliseconds on an accepted string or delay 40 
+milliseconds on a rejected string. Both options allow 
+for delays when reading multi-line input string files.
+
--- nfa/nfa.c
+++ nfa/nfa.c
@@ -74,12 +74,12 @@ char* string_to_bin(char* s) {
 			}
 		}
 	}
-	
+
 	// trim leading zeroes
 	int n;
 	if ((n = strspn(binary, "0")) != 0 && binary[n] != '\0') {        
-        return &binary[n];
-    }
+		return &binary[n];
+	}
 	return binary;
 }
 
@@ -107,7 +107,7 @@ struct State *State_create(char *name, int start, int final)
 	state->zero = malloc(sizeof(struct State *));
 	state->one = malloc(sizeof(struct State *));
 	state->empty = malloc(sizeof(struct State *));
-	
+
 	state->zero_len = 0;
 	state->one_len = 0;
 	state->empty_len = 0;
@@ -122,34 +122,34 @@ struct State *State_create(char *name, int start, int final)
 void State_transition_add(int symbol, struct State *from, struct State *to)
 {
 	if (symbol == 0) {
-	if (from->zero_len == 0) {
-		from->zero[0] = to;
-		from->zero_len++;
-	} else {
-		from->zero_len++;
-		from->zero = realloc(from->zero, sizeof(struct State *) * from->zero_len);
-		from->zero[from->zero_len-1]=to;
-	}
+		if (from->zero_len == 0) {
+			from->zero[0] = to;
+			from->zero_len++;
+		} else {
+			from->zero_len++;
+			from->zero = realloc(from->zero, sizeof(struct State *) * from->zero_len);
+			from->zero[from->zero_len-1]=to;
+		}
 	}
 	if (symbol == 1) {
-	if (from->one_len == 0) {
-		from->one[0] = to;
-		from->one_len++;
-	} else {
-		from->one_len++;
-		from->one = realloc(from->one, sizeof(struct State *) * from->one_len);
-		from->one[from->one_len-1]=to;
-	}
+		if (from->one_len == 0) {
+			from->one[0] = to;
+			from->one_len++;
+		} else {
+			from->one_len++;
+			from->one = realloc(from->one, sizeof(struct State *) * from->one_len);
+			from->one[from->one_len-1]=to;
+		}
 	}
 	if (symbol == 2) {
-	if (from->empty_len == 0) {
-		from->empty[0] = to;
-		from->empty_len++;
-	} else {
-		from->empty_len++;
-		from->empty = realloc(from->empty, sizeof(struct State *) * from->empty_len);
-		from->empty[from->empty_len-1]=to;
-	}
+		if (from->empty_len == 0) {
+			from->empty[0] = to;
+			from->empty_len++;
+		} else {
+			from->empty_len++;
+			from->empty = realloc(from->empty, sizeof(struct State *) * from->empty_len);
+			from->empty[from->empty_len-1]=to;
+		}
 	}
 
 }
@@ -201,18 +201,18 @@ void strip_extra_spaces(char* str) {
 int is_empty(const char *s) {
 	while (*s != '\0') {
 		if (!isspace((unsigned char)*s)) return 0;
-    	s++;
+		s++;
 	}
 	return 1;
 }
 
 /*
-static volatile int keep_running=1;
-void int_handler(int dummy)
-{
-	keep_running = 0;
-}
-*/
+   static volatile int keep_running=1;
+   void int_handler(int dummy)
+   {
+   keep_running = 0;
+   }
+   */
 
 // INITIALIZE FIRST STATE
 struct State *state;
@@ -262,11 +262,11 @@ int nfa_init(char *run_input_string) {
 	nfa_run(run_input_string, state_start);
 
 	// ACCEPTED OR REJECTED?
-//	if (nfa_run(run_input_string) == 1) {
+	//	if (nfa_run(run_input_string) == 1) {
 	if (accepted == 1) {
 		(flag_verbose) && printf("(ACCEPTED):\n\t==>%s\n", input_string);
 		if (flag_verbose && !output_accept_file) print_date();
-	
+
 		// output accepted strings to a supplied file
 		if (output_accept_file != NULL ) {
 			FILE *output_accept_fp;
@@ -290,21 +290,21 @@ int nfa_init(char *run_input_string) {
 		//free(input_string);
 		return 0;
 
-	//} else if (state->final == 0) {
-	
-	//} else if (rejected == 1) {
-	} else {
-		(flag_verbose) && printf("(REJECTED):\n\t==>%s\n", run_input_string);
-		
-		// Delay output on reject?
-		if (flag_verbose && sleep_reject_msec >=0) {
-			if (sleep_reject_msec == 0)
-				sleep_reject_msec = SLEEP_REJECT_MSEC;
-			nsleep(sleep_reject_msec);
-		}
-		//free(input_string);
-		return 1;
+		//} else if (state->final == 0) {
+
+		//} else if (rejected == 1) {
+} else {
+	(flag_verbose) && printf("(REJECTED):\n\t==>%s\n", run_input_string);
+
+	// Delay output on reject?
+	if (flag_verbose && sleep_reject_msec >=0) {
+		if (sleep_reject_msec == 0)
+			sleep_reject_msec = SLEEP_REJECT_MSEC;
+		nsleep(sleep_reject_msec);
 	}
+	//free(input_string);
+	return 1;
+}
 
 }
 
@@ -401,21 +401,21 @@ int main (int argc, char **argv)
 	while( getline(&line, &linelen, machine_fp) > 0 ) {
 		num_lines++;
 		strip_extra_spaces(line);
-		
+
 		int d_start, d_final;
 		char d_name[STATE_NAME_MAX], d_zero[STATE_NAME_MAX], d_one[STATE_NAME_MAX],d_empty[STATE_NAME_MAX];
 		scanned = sscanf(line, "NAME %s START %d FINAL %d ZERO %s ONE %s EMPTY %s",
-					d_name, &d_start, &d_final, d_zero, d_one, d_empty);
+				d_name, &d_start, &d_final, d_zero, d_one, d_empty);
 		if (scanned == 6) {
 			num_states=num_states+1;
-		/* // Machinefile parser debugging
-		} else if (scanned == -1 && is_empty(line)) {
-			printf("Found empty line at line %d\n", num_lines);
-		} else if (scanned == 0 && line[0] == '#') {
-			printf("Found commented line at line %d\n", num_lines);
-		} else {
-			*/
-		// Allow blank and commented (#) lines
+			/* // Machinefile parser debugging
+			   } else if (scanned == -1 && is_empty(line)) {
+			   printf("Found empty line at line %d\n", num_lines);
+			   } else if (scanned == 0 && line[0] == '#') {
+			   printf("Found commented line at line %d\n", num_lines);
+			   } else {
+			   */
+			// Allow blank and commented (#) lines
 		} else if ( !(scanned == -1 && is_empty(line)) && !(scanned == 0 && line[0] == '#')) {
 			line[strcspn(line, "\r\n")] = 0;
 			fprintf(stderr, "Error importing state on line %d:\n%s\n", num_lines, line);
@@ -430,178 +430,178 @@ int main (int argc, char **argv)
 		 s_zero[num_states][STATE_NAME_MAX], 
 		 s_one[num_states][STATE_NAME_MAX],
 		 s_empty[num_states][STATE_NAME_MAX];
-	int s_start[num_states], s_final[num_states], s_zero_indexes[num_states], s_one_indexes[num_states];
+		 int s_start[num_states], s_final[num_states], s_zero_indexes[num_states], s_one_indexes[num_states];
 
-	char state_strings[num_states];
-	line = NULL;
-	linelen = 0;
-	num_lines = 0;
-	int i = 0;
-	while( getline(&line, &linelen, machine_fp) > 0 ) {
-		num_lines++;
-		strip_extra_spaces(line);
-		scanned = sscanf(line, "NAME %s START %d FINAL %d ZERO %s ONE %s EMPTY %s",
-					s_name[i], 
-					&s_start[i], 
-					&s_final[i], 
-					s_zero[i], 
-					s_one[i],
-					s_empty[i]);
-		if (scanned == 6) {
-			i=i+1;
-		// Allow blank and commented (#) lines
-		} else if ( !(scanned == -1 && is_empty(line)) && !(scanned == 0 && line[0] == '#')) {
-			line[strcspn(line, "\r\n")] = 0;
-			fprintf(stderr, "Error importing state on line %d:\n%s\n", num_lines, line);
-			return 3;
-		}
-	}
-	fclose(machine_fp);
+		 char state_strings[num_states];
+		 line = NULL;
+		 linelen = 0;
+		 num_lines = 0;
+		 int i = 0;
+		 while( getline(&line, &linelen, machine_fp) > 0 ) {
+			 num_lines++;
+			 strip_extra_spaces(line);
+			 scanned = sscanf(line, "NAME %s START %d FINAL %d ZERO %s ONE %s EMPTY %s",
+					 s_name[i], 
+					 &s_start[i], 
+					 &s_final[i], 
+					 s_zero[i], 
+					 s_one[i],
+					 s_empty[i]);
+			 if (scanned == 6) {
+				 i=i+1;
+				 // Allow blank and commented (#) lines
+			 } else if ( !(scanned == -1 && is_empty(line)) && !(scanned == 0 && line[0] == '#')) {
+				 line[strcspn(line, "\r\n")] = 0;
+				 fprintf(stderr, "Error importing state on line %d:\n%s\n", num_lines, line);
+				 return 3;
+			 }
+		 }
+		 fclose(machine_fp);
 
-	// Ensure states have unique names
-	for (int i=0; i < num_states; i++) {
-		for (int j=0; j < num_states; j++) {
-			if (!(strcmp(s_name[i], s_name[j])) && i != j) {
-				fprintf(stderr, "Duplicate state names detected in machine file: [%s]\n", s_name[i]);
-				return 4;
-			}
-		}
-	}
+		 // Ensure states have unique names
+		 for (int i=0; i < num_states; i++) {
+			 for (int j=0; j < num_states; j++) {
+				 if (!(strcmp(s_name[i], s_name[j])) && i != j) {
+					 fprintf(stderr, "Duplicate state names detected in machine file: [%s]\n", s_name[i]);
+					 return 4;
+				 }
+			 }
+		 }
 
-	// ENSURE LEGAL NUMBER OF START AND END STATES
-	int starts = 0;
-	int finals = 0;
-	//int i = 0;
-	for (i = 0; i < num_states; i++) {
-		if (s_start[i]) { 
-			starts += 1;
-		}
-		if (starts > 1) {
-			fprintf(stderr, "More than one start state detected. Aborting...\n");
-			return 4;
-		}
-		if (s_final[i]) finals += 1;
-	}
+		 // ENSURE LEGAL NUMBER OF START AND END STATES
+		 int starts = 0;
+		 int finals = 0;
+		 //int i = 0;
+		 for (i = 0; i < num_states; i++) {
+			 if (s_start[i]) { 
+				 starts += 1;
+			 }
+			 if (starts > 1) {
+				 fprintf(stderr, "More than one start state detected. Aborting...\n");
+				 return 4;
+			 }
+			 if (s_final[i]) finals += 1;
+		 }
 
-	if (!starts) {
-		fprintf(stderr, "No start states detected. Aborting...\n");
-		return 4;
-	}
-	if (!finals) {
-		fprintf(stderr, "No final states detected. Aborting...\n");
-		return 4;
-	}
+		 if (!starts) {
+			 fprintf(stderr, "No start states detected. Aborting...\n");
+			 return 4;
+		 }
+		 if (!finals) {
+			 fprintf(stderr, "No final states detected. Aborting...\n");
+			 return 4;
+		 }
 
-	// CREATE ARRAY OF STATES
-	struct State *states[num_states];
+		 // CREATE ARRAY OF STATES
+		 struct State *states[num_states];
 
-	// FILL ARRAY WITH STATE NAMES, START, FINAL
-	for (i = 0; i < num_states; i++) {
-		struct State *new_state = State_create(s_name[i], 
-				s_start[i], 
-				s_final[i]);
-		states[i] = new_state;
+		 // FILL ARRAY WITH STATE NAMES, START, FINAL
+		 for (i = 0; i < num_states; i++) {
+			 struct State *new_state = State_create(s_name[i], 
+					 s_start[i], 
+					 s_final[i]);
+			 states[i] = new_state;
 
-		// Set start state
-		if (new_state->start) state_start = new_state;
-	}
+			 // Set start state
+			 if (new_state->start) state_start = new_state;
+		 }
 
-	// FILL STATE ARRAY WITH TRANSITIONS
-	for (i = 0; i < num_states; i++) {
-		char *state_zero = s_zero[i];
-		char *state_one = s_one[i];
-		char *state_empty = s_empty[i];
-		
-		if (strcasecmp(state_zero, "x") != 0) {
-			char *single_zero;
-			single_zero = strtok(state_zero, ",");
-			while(single_zero != NULL) {
-				//printf("zero_state: %s\n", single_zero);
-				int j;
-				for (j=0; strcmp(single_zero, states[j]->name); j++);
-				State_transition_add(0, states[i], states[j]);
+		 // FILL STATE ARRAY WITH TRANSITIONS
+		 for (i = 0; i < num_states; i++) {
+			 char *state_zero = s_zero[i];
+			 char *state_one = s_one[i];
+			 char *state_empty = s_empty[i];
 
-				single_zero = strtok(NULL, ",");
-			}
-		}
+			 if (strcasecmp(state_zero, "x") != 0) {
+				 char *single_zero;
+				 single_zero = strtok(state_zero, ",");
+				 while(single_zero != NULL) {
+					 //printf("zero_state: %s\n", single_zero);
+					 int j;
+					 for (j=0; strcmp(single_zero, states[j]->name); j++);
+					 State_transition_add(0, states[i], states[j]);
 
-		if (strcasecmp(state_one, "x") != 0) {
-			char *single_one;
-			single_one = strtok(state_one, ",");
-			while(single_one != NULL) {
-				//printf("one_state: %s\n", single_one);
-				int j;
-				for (j=0; strcmp(single_one, states[j]->name); j++);
-				State_transition_add(1, states[i], states[j]);
+					 single_zero = strtok(NULL, ",");
+				 }
+			 }
 
-				single_one = strtok(NULL, ",");
-			}
-		}
+			 if (strcasecmp(state_one, "x") != 0) {
+				 char *single_one;
+				 single_one = strtok(state_one, ",");
+				 while(single_one != NULL) {
+					 //printf("one_state: %s\n", single_one);
+					 int j;
+					 for (j=0; strcmp(single_one, states[j]->name); j++);
+					 State_transition_add(1, states[i], states[j]);
 
-		if (strcasecmp(state_empty, "x") != 0) {
-			char *single_empty;
-			single_empty = strtok(state_empty, ",");
-			while(single_empty != NULL) {
-				//printf("empty_state: %s\n", single_empty);
-				int j;
-				for (j=0; strcmp(single_empty, states[j]->name); j++);
-				State_transition_add(2, states[i], states[j]);
+					 single_one = strtok(NULL, ",");
+				 }
+			 }
 
-				single_empty = strtok(NULL, ",");
-			}
-		}
+			 if (strcasecmp(state_empty, "x") != 0) {
+				 char *single_empty;
+				 single_empty = strtok(state_empty, ",");
+				 while(single_empty != NULL) {
+					 //printf("empty_state: %s\n", single_empty);
+					 int j;
+					 for (j=0; strcmp(single_empty, states[j]->name); j++);
+					 State_transition_add(2, states[i], states[j]);
 
-		if (flag_verbose) State_print(states[i]);
+					 single_empty = strtok(NULL, ",");
+				 }
+			 }
 
-	}
+			 if (flag_verbose) State_print(states[i]);
 
-	if (input_string_file && !input_string)
-	{
-		// READ INPUT STRING(S) FROM FILE
-		FILE *input_string_fp;
-		char *input_string_bits = NULL;
-		size_t len = 0;
-		ssize_t read;
+		 }
 
-		input_string_fp = fopen(input_string_file, "r");
-		if (input_string_fp == NULL) {
-			fprintf(stderr, "Error opening %s\n", input_string_file);
-			return 3;
-		}
+		 if (input_string_file && !input_string)
+		 {
+			 // READ INPUT STRING(S) FROM FILE
+			 FILE *input_string_fp;
+			 char *input_string_bits = NULL;
+			 size_t len = 0;
+			 ssize_t read;
 
-		while ((read = getline(&input_string_bits, &len, input_string_fp)) != -1)
-		{
-			input_string = input_string_bits;
-			input_string[strcspn(input_string, "\r\n")] = 0;
-		
-			if (flag_string) {
-				char *input_ascii = string_to_bin(input_string);
-				input_string = input_ascii;
-			}
-			state = state_start;
-			nfa_init(input_string);
-			
-			/*
-			signal(SIGINT, int_handler);
-			while(!keep_running) {
-				fclose(input_string_fp);
-				States_destroy(states, num_states);
-				return 0;
-			}
-			*/
-		}
-		fclose(input_string_fp);
+			 input_string_fp = fopen(input_string_file, "r");
+			 if (input_string_fp == NULL) {
+				 fprintf(stderr, "Error opening %s\n", input_string_file);
+				 return 3;
+			 }
 
-	} else {
-		if (flag_string) {
-			char *input_ascii = string_to_bin(input_string);
-			input_string = input_ascii;
-		}
-		state = state_start;
-		nfa_init(input_string);
-	}
+			 while ((read = getline(&input_string_bits, &len, input_string_fp)) != -1)
+			 {
+				 input_string = input_string_bits;
+				 input_string[strcspn(input_string, "\r\n")] = 0;
 
-	States_destroy(states, num_states);
+				 if (flag_string) {
+					 char *input_ascii = string_to_bin(input_string);
+					 input_string = input_ascii;
+				 }
+				 state = state_start;
+				 nfa_init(input_string);
 
-	return 0;
+				 /*
+					signal(SIGINT, int_handler);
+					while(!keep_running) {
+					fclose(input_string_fp);
+					States_destroy(states, num_states);
+					return 0;
+					}
+					*/
+			 }
+			 fclose(input_string_fp);
+
+		 } else {
+			 if (flag_string) {
+				 char *input_ascii = string_to_bin(input_string);
+				 input_string = input_ascii;
+			 }
+			 state = state_start;
+			 nfa_init(input_string);
+		 }
+
+		 States_destroy(states, num_states);
+
+		 return 0;
 }
